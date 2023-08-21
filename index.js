39bc9cfda4c21d87916fbac3c1cfa4810149c92f
Chacha <chachazw@gmail.com>
2023-08-22 00:08:36 +0800

    chore: add shim js and settings.json, update Podfile.lock

--- index.js
+++ index.js
@@ -2,8 +2,10 @@
  * @format
  */
 
+import 'react-native-get-random-values';
 import {AppRegistry} from 'react-native';
 import App from './App';
+import './shim';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
