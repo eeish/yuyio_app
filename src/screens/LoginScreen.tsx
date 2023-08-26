a13fb5c16d8a32c8e53eed9474b54ce18bbe128a
Chacha <chachazw@gmail.com>
2023-08-26 20:43:49 +0800

    feat: update LoginScreen component and add google login

--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -2,12 +2,15 @@ import {
   WalletConnectModal,
   useWalletConnectModal,
 } from '@walletconnect/modal-react-native';
+import { ethers } from 'ethers';
 import { useCallback, useEffect } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
 import Button from '../components/Button';
 import Container from '../components/Container';
+import { LOGIN_REDIRECT_URL } from '../utils/constants';
 import { Routes } from '../utils/navigation';
 import { RootStackScreenProps } from '../utils/types';
+import web3auth from '../utils/web3auth';
 import web3ModalConfig from '../utils/web3modal';
 
 const LoginScreen = ({ navigation }: RootStackScreenProps<'LoginScreen'>) => {
@@ -27,16 +30,51 @@ const LoginScreen = ({ navigation }: RootStackScreenProps<'LoginScreen'>) => {
     }
   }, [isConnected, open, provider]);
 
+  const onPressGoogleLogin = async () => {
+    try {
+      await web3auth.init();
+      await web3auth.login({
+        loginProvider: 'google',
+        redirectUrl: LOGIN_REDIRECT_URL,
+        mfaLevel: 'none',
+        curve: 'secp256k1',
+      });
+
+      if (!web3auth.privKey) {
+        throw new Error('No private key retrieved from web3auth');
+      }
+
+      const wallet = new ethers.Wallet(web3auth.privKey);
+      const address = await wallet.getAddress();
+      const userInfo = web3auth.userInfo();
+
+      console.log('address', address);
+      console.log('userInfo', userInfo);
+
+      navigation.navigate(Routes.TopStack.MapScreen);
+    } catch (error) {
+      console.log('error', error);
+    }
+  };
+
   return (
     <Container source={require('../assets/images/splash.jpg')}>
       <View style={styles.container}>
         <Text style={styles.welcome}>Welcome!</Text>
       </View>
       <View style={styles.buttonWrapper}>
+        <Button onPress={onPressConnect} text={'Connect Wallet'} />
         <Button
-          style={styles.walletButton}
-          onPress={onPressConnect}
-          text={'Connect Wallet'}
+          textStyle={styles.textStyle}
+          style={styles.googleButton}
+          onPress={onPressGoogleLogin}
+          text={'Login with Google'}
+          prefix={
+            <Image
+              style={styles.googleIcon}
+              source={require('../assets/images/google.png')}
+            />
+          }
         />
       </View>
       <WalletConnectModal {...web3ModalConfig} />
@@ -54,11 +92,23 @@ const styles = StyleSheet.create({
     fontSize: 40,
   },
   buttonWrapper: {
-    paddingHorizontal: 16,
-  },
-  walletButton: {
     position: 'absolute',
+    left: 0,
+    right: 0,
     bottom: 64,
+    paddingHorizontal: 16,
+    gap: 24,
+  },
+  googleButton: {
+    backgroundColor: 'white',
+  },
+  googleIcon: {
+    width: 24,
+    height: 24,
+    marginRight: 8,
+  },
+  textStyle: {
+    color: 'black',
   },
 });
 
