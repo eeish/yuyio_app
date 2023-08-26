5760d518c722547949ebf40826df34eae1050b32
Chacha <chachazw@gmail.com>
2023-08-26 19:59:22 +0800

    feat: update metro and babel config, update package.json

--- metro.config.js
+++ metro.config.js
@@ -5,13 +5,19 @@
  * @format
  */
 
-const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
 
 /**
  * Metro configuration
  * https://facebook.github.io/metro/docs/configuration
  * @type {import('metro-config').MetroConfig}
  */
-const config = {};
+const config = {
+  resolver: {
+    extraNodeModules: {
+      ...require('expo-crypto-polyfills'),
+    },
+  },
+};
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
